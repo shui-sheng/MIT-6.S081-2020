2d6c0be0d230471eed8bff2089067bb1125b971f
zyf <123.com>
2023-07-06 21:13:06 +0800

    lab2

--- kernel/proc.c
+++ kernel/proc.c
@@ -291,6 +291,7 @@ fork(void)
 
   safestrcpy(np->name, p->name, sizeof(p->name));
 
+  np->mask = p->mask; //lab added
   pid = np->pid;
 
   np->state = RUNNABLE;
@@ -693,3 +694,27 @@ procdump(void)
     printf("\n");
   }
 }
+// Return the number of processes whose state is not UNUSED
+uint64
+nproc(void)
+{
+  struct proc *p;
+  // counting the number of processes
+  uint64 num = 0;
+  // traverse all processes
+  for (p = proc; p < &proc[NPROC]; p++)
+  {
+    // add lock
+    acquire(&p->lock);
+    // if the processes's state is not UNUSED
+    if (p->state != UNUSED)
+    {
+      // the num add one
+      num++;
+    }
+    // release lock
+    release(&p->lock);
+  }
+  return num;
+}
+
